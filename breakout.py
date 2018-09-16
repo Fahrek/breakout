61147a69bca9c630855217dc723a8e988941e8d3
Fahrek <andresgqjob@gmail.com>
2018-09-16 19:01:27 +0200

    The Collitions

--- breakout.py
+++ breakout.py
@@ -1,34 +1,59 @@
-import sys #para usar exit()
+import sys  # para usar exit()
 import pygame
 
-WIDTH  = 640
+WIDTH = 640
 HEIGHT = 480
+bgBlueCol = (0, 0, 64)  # Color azul para el fondo
+
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        #Cargar imagen
+        # Cargar imagen
         self.image = pygame.image.load('assets/ball.png')
-        #Obtener rectangulo de la imagen
-        self.rect  = self.image.get_rect()
-        #Posicion inicial centrada en pantalla
+        # Obtener rectangulo de la imagen
+        self.rect = self.image.get_rect()
+        # Posicion inicial centrada en pantalla
         self.rect.centerx = WIDTH/2
         self.rect.centery = HEIGHT/2
+        # Establecer velocidad inicial
+        self.speed = [3, 3]
 
-#Inicializando pantalla
+    def update(self):
+        # Evitar que salga por arriba y por abajo
+        if self.rect.bottom >= HEIGHT or self.rect.top <= 0:
+            self.speed[1] = -self.speed[1]
+        # Evitar que salga por la derecha y por la izquierda
+        if self.rect.right >= WIDTH or self.rect.left <= 0:
+            self.speed[0] = -self.speed[0]
+        # Mover en base a posicion actual y velocidad
+        self.rect.move_ip(self.speed)
+
+# Inicializando pantalla
+
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-#Configurar título de la pantalla
+# Configurar título de la pantalla
 pygame.display.set_caption('BreakOut')
+# Crear el reloj
+clock = pygame.time.Clock()
 
 ball = Ball()
 
 while True:
-    #Revisar todos los eventos
+    # Establecer FPS
+    clock.tick(60)
+    # Revisar todos los eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
-    pygame.display.flip()
 
-    #Dibujar bola en pantalla
+    # Actualizar la posicion de la bola
+    ball.update()
+
+    # Rellenar fondo de pantalla con el color azul
+    screen.fill(bgBlueCol)
+    # Dibujar bola en pantalla
     screen.blit(ball.image, ball.rect)
-    #Actualizar los elementos en pantalla
+    # Actualizar los elementos en pantalla
+    pygame.display.flip()
