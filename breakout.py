7794eccf9547f29f442c81d6f2f5638fda75d264
Fahrek <andresgqjob@gmail.com>
2018-09-16 04:20:57 +0200

    Create The Ball

--- breakout.py
+++ breakout.py
@@ -1,14 +1,26 @@
 import sys #para usar exit()
 import pygame
 
-ANCHO = 640
-ALTO  = 480
+WIDTH  = 640
+HEIGHT = 480
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #Cargar imagen
+        self.image = pygame.image.load('assets/ball.png')
+        #Obtener rectangulo de la imagen
+        self.rect  = self.image.get_rect()
+        #Posicion inicial centrada en pantalla
+        self.rect.centerx = WIDTH/2
+        self.rect.centery = HEIGHT/2
 
 #Inicializando pantalla
-pantalla = pygame.display.set_mode((ANCHO, ALTO))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 #Configurar título de la pantalla
 pygame.display.set_caption('BreakOut')
 
+ball = Ball()
 
 while True:
     #Revisar todos los eventos
@@ -16,3 +28,7 @@ while True:
         if evento.type == pygame.QUIT:
             sys.exit()
     pygame.display.flip()
+
+    #Dibujar bola en pantalla
+    screen.blit(ball.image, ball.rect)
+    #Actualizar los elementos en pantalla
