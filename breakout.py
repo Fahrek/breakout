ffecf972787e3fb4df9807fe25ebd2a6d974c46b
Fahrek <andresgqjob@gmail.com>
2018-09-17 05:28:43 +0200

    Stage One Scene & Game Over Scene Implements

--- breakout.py
+++ breakout.py
@@ -2,48 +2,49 @@ import sys  # para usar exit()
 import time  # para usar sleep()
 import pygame
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 640  # Ancho de la pantalla.
+HEIGHT = 480  # Alto de la pantalla.
 bgBlueCol = (0, 0, 64)  # Color azul para el fondo
 white_color = (255, 255, 255)  # Color blanco para textos
 
-pygame.init()
 
 class Scene:
+    "Esqueleto para cada una de las escenas del videojuego."
     def __init__(self):
-        "Initialization"
+        "Inicializacion"
         self.nextScene = False
         self.playing = True
 
     def read_events(self, events):
-        "Lee la lista de los todos eventos"
+        "Lee los eventos para interactuar con los objetos."
         pass
 
     def update(self):
-        "Calculos y logica"
+        "Calculos y logica. Actualiza los objetos en la pantalla."
         pass
 
     def render(self, screen):
-        "Dibujar los objetos en pantalla"
+        "Dibuja los objetos en pantalla"
         pass
 
     def change_scene(self, scene):
-        "Selecciona la nueva escena a ser desplegada"
+        "Selecciona la nueva escena a ser desplegada. Cambia la escena de juego"
         self.nextScene = scene
 
 
 class Director:
-    def __init__(self, title = "", res = (WIDTH, HEIGHT)):
+    def __init__(self, title="", res=(WIDTH, HEIGHT)):
         pygame.init()
+        # Inicializando pantalla
         self.screen = pygame.display.set_mode(res)
-        # Configurar título de la pantalla
+        # Configurar titulo de la pantalla
         pygame.display.set_caption(title)
         # Crear el reloj
         self.clock = pygame.time.Clock()
         self.scene = None
         self.scenes = {}
 
-    def execute(self, init_scene, fps = 60):
+    def execute(self, init_scene, fps=60):
         self.scene = self.scenes[init_scene]
         playing = True
         while playing:
@@ -65,6 +66,7 @@ class Director:
             if playing:
                 playing = self.scene.playing
 
+            # Actualizar los elementos en pantalla
             pygame.display.flip()
 
         time.sleep(3)
@@ -76,9 +78,109 @@ class Director:
             self.scene = self.scenes[nextScene]
 
     def addScene(self, scene):
-        sceneClass = 'STAGE' + scene
+        sceneClass = 'Scene' + scene
         sceneObj = globals()[sceneClass]
         self.scenes[scene] = sceneObj()
+
+
+class SceneStage1(Scene):
+    def __init__(self):
+        Scene.__init__(self)
+
+        self.ball = Ball()
+        self.player = Paddle()
+        self.wall = Wall(50)
+
+        self.points = 0
+        self.lives = 3
+        self.waiting_tOut = True
+
+        # Ajustar repeticion de evento de tecla presionada
+        pygame.key.set_repeat(30)
+
+    def read_events(self, events):
+        for event in events:
+            # Buscar eventos del teclado,
+            if event.type == pygame.KEYDOWN:
+                self.player.update(event)
+                if self.waiting_tOut == True and event.key == pygame.K_SPACE:
+                    self.waiting_tOut = False
+                    if self.ball.rect.centerx < WIDTH / 2:
+                        self.ball.speed = [3, -3]
+                    else:
+                        self.ball.speed = [-3, -3]
+
+    def update(self):
+        # Actualizar la posicion de la bola (OJO: if self.waiting_tOut == False:)
+        if not self.waiting_tOut:
+            self.ball.update()
+        else:
+            self.ball.rect.midbottom = self.player.rect.midtop
+
+        # Colision entre la bola y el jugador
+        if pygame.sprite.collide_rect(self.ball, self.player):
+            self.ball.speed[1] = -self.ball.speed[1]
+
+        # Colision de la bola con el muro
+        list = pygame.sprite.spritecollide(self.ball, self.wall, False)
+        if list:
+            brick = list[0]
+            cx = self.ball.rect.centerx
+            if cx < brick.rect.left or cx > brick.rect.right:
+                self.ball.speed[0] = -self.ball.speed[0]
+            else:
+                self.ball.speed[1] = -self.ball.speed[1]
+            self.wall.remove(brick)
+            self.points += 10
+
+        # Revisar si la bola sale de la pantalla
+        if self.ball.rect.top > HEIGHT:
+            self.lives -= 1
+            self.waiting_tOut = True
+
+        if self.lives <= 0:
+            self.change_scene('GameOver')
+
+    def render(self, screen):
+        # Rellenar fondo de pantalla con el color azul
+        screen.fill(bgBlueCol)
+        # Mostrar puntuacion
+        self.show_points(screen)
+        # Mostrar vidas
+        self.show_lives(screen)
+        # Dibujar bola en pantalla
+        screen.blit(self.ball.image, self.ball.rect)
+        # Dibujar jugador en pantalla
+        screen.blit(self.player.image, self.player.rect)
+        # Dibujar los ladrillos
+        self.wall.draw(screen)
+
+    def show_points(self, screen):
+        font = pygame.font.SysFont('Consolas', 20)
+        text = font.render(str(self.points).zfill(5), True, white_color)
+        text_rect = text.get_rect()
+        text_rect.topleft = [0, 0]
+        screen.blit(text, text_rect)
+
+    def show_lives(self, screen):
+        font = pygame.font.SysFont('Consolas', 20)
+        chain = "Vidas: " + str(self.lives).zfill(2)
+        text = font.render(chain, True, white_color)
+        text_rect = text.get_rect()
+        text_rect.topright = [WIDTH, 0]
+        screen.blit(text, text_rect)
+
+
+class GameOverScene(Scene):
+    def update(self):
+        self.playing = False
+
+    def render(self, screen):
+        font = pygame.font.SysFont('Arial', 72)
+        text = font.render('GAME OVER :(', True, (255, 255, 255))
+        text_rect = text.get_rect()
+        text_rect.center = [WIDTH / 2, HEIGHT / 2]
+        screen.blit(text, text_rect)
 
 
 class Ball(pygame.sprite.Sprite):
@@ -156,119 +258,9 @@ class Wall(pygame.sprite.Group):
                 pos_x = 0
                 pos_y += brick.rect.height
 
-# Funcion llamada tras dejar ir la bola
+
+director = Director('BreakOut', (WIDTH, HEIGHT))
+director.addScene('Stage1')
+director.execute('Stage1')
 
 
-def game_over():
-    font = pygame.font.SysFont('Arial', 72)
-    text = font.render('GAME OVER :(', True, (255, 255, 255))
-    text_rect = text.get_rect()
-    text_rect.center = [WIDTH / 2, HEIGHT / 2]
-    screen.blit(text, text_rect)
-    pygame.display.flip()
-    # Pausar por tres segundos
-    time.sleep(3)
-    # Salir
-    sys.exit()
-
-
-def show_points():
-    font = pygame.font.SysFont('Consolas', 20)
-    text = font.render(str(points).zfill(5), True, white_color)
-    text_rect = text.get_rect()
-    text_rect.topleft = [0, 0]
-    screen.blit(text, text_rect)
-
-
-def show_lives():
-    font = pygame.font.SysFont('Consolas', 20)
-    chain = "Vidas: " + str(lives).zfill(2)
-    text = font.render(chain, True, white_color)
-    text_rect = text.get_rect()
-    text_rect.topright = [WIDTH, 0]
-    screen.blit(text, text_rect)
-
-
-# Inicializando pantalla
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# Configurar título de la pantalla
-pygame.display.set_caption('BreakOut')
-# Crear el reloj
-clock = pygame.time.Clock()
-# Ajustar repetición de evento de tecla presionada
-pygame.key.set_repeat(30)
-
-
-# Instanciando objetos en pantalla
-
-ball = Ball()
-player = Paddle()
-wall = Wall(50)
-points = 0
-lives = 3
-waiting_tOut = True
-
-while True:
-    # Establecer FPS
-    clock.tick(60)
-    # Revisar todos los eventos
-    for event in pygame.event.get():
-        # Si se presiona la cruz de la barra de titulo,
-        if event.type == pygame.QUIT:
-            # cerrar el videojuego
-            sys.exit()
-        # Buscar eventos del teclado,
-        elif event.type == pygame.KEYDOWN:
-            player.update(event)
-            if waiting_tOut == True and event.key == pygame.K_SPACE:
-                waiting_tOut = False
-                if ball.rect.centerx < WIDTH / 2:
-                    ball.speed = [3, -3]
-                else:
-                    ball.speed = [-3, -3]
-
-
-    # Actualizar la posicion de la bola
-    if waiting_tOut == False:
-        ball.update()
-    else:
-        ball.rect.midbottom = player.rect.midtop
-
-    # Colision entre la bola y el jugador
-    if pygame.sprite.collide_rect(ball, player):
-        ball.speed[1] = -ball.speed[1]
-
-    # Colision de la bola con el muro
-    list = pygame.sprite.spritecollide(ball, wall, False)
-    if list:
-        brick = list[0]
-        cx = ball.rect.centerx
-        if cx < brick.rect.left or cx > brick.rect.right:
-            ball.speed[0] = -ball.speed[0]
-        else:
-            ball.speed[1] = -ball.speed[1]
-        wall.remove(brick)
-        points += 10
-
-    # Revisar si la bola sale de la pantalla
-    if ball.rect.top > HEIGHT:
-        lives -= 1
-        waiting_tOut = True
-
-    # Rellenar fondo de pantalla con el color azul
-    screen.fill(bgBlueCol)
-    # Mostrar puntuacion
-    show_points()
-    # Mostrar vidas
-    show_lives()
-    # Dibujar bola en pantalla
-    screen.blit(ball.image, ball.rect)
-    # Dibujar jugador en pantalla
-    screen.blit(player.image, player.rect)
-    # Dibujar los ladrillos
-    wall.draw(screen)
-    # Actualizar los elementos en pantalla
-    pygame.display.flip()
-
-    if lives <= 0:
-        game_over()
